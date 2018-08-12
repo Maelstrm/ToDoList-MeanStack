7f012215ade8e4d4e483968e2f972e71476fefb5
Jakeh Clark <jakehclarkcreates@gmail.com>
2018-08-12 17:05:48 -0500

    server up

---
+++ server/server.js
@@ -0,0 +1,36 @@
+// Require
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000;
+
+// Require toDoListRouter into server
+const toDoListRouter = require('./router/toDoListRouter.js')
+
+// Require mongoose
+const mongoose = require('mongoose');
+const mongoURI = 'mongomongodb://localhost:27017/todolist';
+
+// Attempt to connect to DB
+mongoose.connect(mongoURI, { useNewUrlParser: true});
+// Log success/Failure in terminal
+mongoose.connection.on('open', () => {
+    console.log('connected to mongo');
+})
+mongoose.connection.on('error', (error) => {
+    console.log(error);
+})
+
+// Configure body-parser for Angular and jQuery
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); // Required for Angular
+
+// Static files
+app.use(express.static('server/public'));
+app.use('toDoList', toDoListRouter);
+
+// Listen to requests from specific port
+app.listen(PORT, () => {
+    console.log('listening to port', PORT);
+
+})
